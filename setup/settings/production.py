ffeff7b96b722fd7dea31f5a5a284be262d357e8
PUYUP <hellopuyup@gmail.com>
2021-06-24 20:49:40 +0700

    allowed cors

--- setup/settings/production.py
+++ setup/settings/production.py
@@ -70,6 +70,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost',
     'http://localhost:8100',
+    'https://app.daftarbelanja.com',
 ]
 
 CORS_ALLOW_HEADERS = list(default_headers) + [
